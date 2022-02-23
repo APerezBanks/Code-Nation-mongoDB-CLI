9f4af37bf702eb7e71877fe3b8aecf3719220b70
APerezBanks <88187553+APerezBanks@users.noreply.github.com>
2022-02-23 15:32:17 +0000

    cant manage to get it to work

--- src/app.js
+++ src/app.js
@@ -10,11 +10,22 @@ const app = async (yargsObj) => {
             const movie = new Movie (yargsObj.title, yargsObj.actor, yargsObj.year);
             console.log(await movie.add(collection));
             // take movie info, add it to the mogodb database and cosole.log a success message
+
         } else if (yargsObj.list) {
             const movie = new Movie (yargsObj.title, yargsObj.actor, yargsObj.year);
             console.log(await movie.list(collection))
             //list all movies in database
         
+
+
+     // added to do  challenge bellow
+
+        } else if (yargsObj.updateOne) {
+            const movie = new Movie (yargsObj.title, yargsObj.actor, yargsObj.year);
+            console.log(await movie.update(collection))
+
+    // update on object in the list
+            
     } else {
         console.log("Incorrect command")
     }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,21 +1,101 @@
+const { MongoDBNamespace } = require("mongodb");
+
 class Movie {
     constructor ( title, actor, year = "Not specified" ) {
         this.title = title;
         this.actor = actor;
-        // added for task
+
+    // added for task ///
         this.year = year;
+
     }
+
+
+
+
+    // Create (CRUD)  add this to the database
+
     async add (collection) {
-        //add this to the database
         await collection.insertOne(this);
         return "Success";
     }
 
+    //READ (CRUD) list all movies in the db
+
     async list (collection){
             return await  collection.find().toArray();
-            // list all movies in the db
+           
           
-        }
+    }
+
+
+ // attemp numbert 1 to update
+
+//  set async update(collection) {
+//     this.title = collection.updateOne()
+//     this.actor = collection.updateOne()
+//     this.year = collection.updateOne();}
+
+// attemp number 2 to update
+    
+    // collection(movies).updateOne({_id; new MongoDBNamespace.objectid("")},{$set;{title: Jhon}})
+
+
+
+// attemp number 3 
+
+    // async updateOne(collection, var1, var2, var3) {
+    //     console.log("var1 is: ", var1 );
+    //     console.log("var2 is: ", var2 );
+    //     return await collection.updateOne(this, {
+           
+    //       $set: {
+            
+    //         title: var1,
+    //         actor: var2,
+    //         year: var3
+            
+    //       },
+    //     });
+    // };
+
+  //Update (CRUD) attemp 4
+
+    // async update (collection) {
+    //     console.log(update)
+    //     await collection.updateOne( 
+    //         { title : this.title }, 
+    //         { $set : {"actor" : this.actor} },
+    //         { $set : {"year" : this.year} }
+    //         );
+    //     return await  collection.find().toArray();
+    // }
+
+// attemp to delete 01
+
+            // async query = { title: "Annie Hall" };
+            // async result = await movies.deleteOne(query);
+            //     if (result.deletedCount === 1) {
+            //                     console.log("Successfully deleted one document.");
+            //             } else {
+            //                      console.log("No documents matched the query. Deleted 0 documents.");
+            //                     }
+
+
+//Delete (CRUD)
+
+
+
+    
+
+
+
 }
 
-module.exports = Movie;
+    module.exports = Movie;
+
+
+
+
+
+                                 
